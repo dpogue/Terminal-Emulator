eb012e41ac3ffa0ac9a40648644064ea7e302fba
Darryl Pogue <dvpdiner2@gmail.com>
2010-11-24 09:32:00 -0800

    Fixed CRC header from Terry

--- src/emulation/wireless/crc.h
+++ src/emulation/wireless/crc.h
@@ -20,53 +20,15 @@
 #define FALSE	0
 #define TRUE	!FALSE
 
-/*
- * Select the CRC standard from the list that follows.
- */
-#define CRC_CCITT
-
-
-#if defined(CRC_CCITT)
-
 typedef unsigned short  crc;
 
 #define CRC_NAME			"CRC-CCITT"
 #define POLYNOMIAL			0x107
-#define INITIAL_REMAINDER	0xFFFF
-#define FINAL_XOR_VALUE		0x0000
+#define INITIAL_REMAINDER	0x00
+#define FINAL_XOR_VALUE		0x00
 #define REFLECT_DATA		FALSE
 #define REFLECT_REMAINDER	FALSE
-#define CHECK_VALUE			0x29B1
-
-#elif defined(CRC16)
-
-typedef unsigned short  crc;
-
-#define CRC_NAME			"CRC-16"
-#define POLYNOMIAL			0x107
-#define INITIAL_REMAINDER	0x0000
-#define FINAL_XOR_VALUE		0x0000
-#define REFLECT_DATA		TRUE
-#define REFLECT_REMAINDER	TRUE
-#define CHECK_VALUE			0xBB3D
-
-#elif defined(CRC32)
-
-typedef unsigned long  crc;
-
-#define CRC_NAME			"CRC-32"
-#define POLYNOMIAL			0x107
-#define INITIAL_REMAINDER	0xFFFFFFFF
-#define FINAL_XOR_VALUE		0xFFFFFFFF
-#define REFLECT_DATA		TRUE
-#define REFLECT_REMAINDER	TRUE
-#define CHECK_VALUE			0xCBF43926
-
-#else
-
-#error "One of CRC_CCITT, CRC16, or CRC32 must be #define'd."
-
-#endif
+#define CHECK_VALUE		    0xF4
 
 
 void  crcInit(void);
